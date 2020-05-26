399a260dd847b60a61340cf8a9ca4f5bbe4e3298
jacobkap <jacobkap@sas.upenn.edu>
2020-05-25 23:01:55 -0400

    working on budget page

--- js/main.js
+++ js/main.js
@@ -71,3 +71,34 @@ function makeTable(type) {
   });
   return temp_table;
 }
+
+// Licensed under the MIT license (https://raw.githubusercontent.com/jhovgaard/jquery.rangegroup/master/LICENSE)
+
+$(document).on('input', 'input[range-group]', function() {
+  selectedRange = $(this);
+  var group = selectedRange.attr('range-group');
+  var max = selectedRange.attr('range-group-max-sum');
+
+  var ranges = $('input[range-group=' + group + ']');
+  var total = 0;
+  ranges.each(function(i, item) {
+    total += parseInt($(item).val());
+  });
+
+  var left = max - total;
+
+  if(left < 0) {
+
+    var half = Math.ceil(left / 2);
+
+    ranges.each(function(i, item) {
+      $item = $(item);
+      if ($item.get(0) === selectedRange.get(0)) {
+        return true;
+      };
+      var newValue = parseInt($item.val()) + half;
+      $item.val(newValue);
+      $item.trigger('change');
+    });
+  }
+});
